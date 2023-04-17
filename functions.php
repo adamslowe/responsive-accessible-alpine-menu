25aa643eeb446e82f2859938c5380b04e053bfe0
adamslowe <lowe.adam@gmail.com>
2023-04-17 16:39:10 -0400

    Changed to Alpine

--- functions.php
+++ functions.php
@@ -152,22 +152,18 @@ if ( ! function_exists( 'pgmenu_enqueue_scripts' ) ) :
 
         /* Pinegrow generated Enqueue Scripts Begin */
 
-    wp_register_script( 'inline-script-1', '', [], '0.0.7', false );
-    wp_enqueue_script( 'inline-script-1' );
-    wp_add_inline_script( 'inline-script-1', '/* Pinegrow Interactions, do not remove */ (function(){try{if(!document.documentElement.hasAttribute(\'data-pg-ia-disabled\')) { window.pgia_small_mq=typeof pgia_small_mq==\'string\'?pgia_small_mq:\'(max-width:767px)\';window.pgia_large_mq=typeof pgia_large_mq==\'string\'?pgia_large_mq:\'(min-width:768px)\';var style = document.createElement(\'style\');var pgcss=\'html:not(.pg-ia-no-preview) [data-pg-ia-hide=""] {opacity:0;visibility:hidden;}html:not(.pg-ia-no-preview) [data-pg-ia-show=""] {opacity:1;visibility:visible;display:block;}\';if(document.documentElement.hasAttribute(\'data-pg-id\') && document.documentElement.hasAttribute(\'data-pg-mobile\')) {pgia_small_mq=\'(min-width:0)\';pgia_large_mq=\'(min-width:99999px)\'} pgcss+=\'@media \' + pgia_small_mq + \'{ html:not(.pg-ia-no-preview) [data-pg-ia-hide="mobile"] {opacity:0;visibility:hidden;}html:not(.pg-ia-no-preview) [data-pg-ia-show="mobile"] {opacity:1;visibility:visible;display:block;}}\';pgcss+=\'@media \' + pgia_large_mq + \'{html:not(.pg-ia-no-preview) [data-pg-ia-hide="desktop"] {opacity:0;visibility:hidden;}html:not(.pg-ia-no-preview) [data-pg-ia-show="desktop"] {opacity:1;visibility:visible;display:block;}}\';style.innerHTML=pgcss;document.querySelector(\'head\').appendChild(style);}}catch(e){console&&console.log(e);}})()');
-
-    wp_deregister_script( 'pgmenu-pgia' );
-    wp_enqueue_script( 'pgmenu-pgia', get_template_directory_uri() . '/pgia/lib/pgia.js', [], '0.0.7', true);
+    wp_deregister_script( 'pgmenu-alpinewithfocus' );
+    wp_enqueue_script( 'pgmenu-alpinewithfocus', get_template_directory_uri() . '/js/alpine-with-focus.js', [], '0.0.8', true);
 
     /* Pinegrow generated Enqueue Scripts End */
 
         /* Pinegrow generated Enqueue Styles Begin */
 
     wp_deregister_style( 'pgmenu-style' );
-    wp_enqueue_style( 'pgmenu-style', get_template_directory_uri() . '/css/style.css', [], '0.0.7', 'all');
+    wp_enqueue_style( 'pgmenu-style', get_template_directory_uri() . '/css/style.css', [], '0.0.8', 'all');
 
     wp_deregister_style( 'pgmenu-style-1' );
-    wp_enqueue_style( 'pgmenu-style-1', get_bloginfo('stylesheet_url'), [], '0.0.7', 'all');
+    wp_enqueue_style( 'pgmenu-style-1', get_bloginfo('stylesheet_url'), [], '0.0.8', 'all');
 
     /* Pinegrow generated Enqueue Styles End */
 
